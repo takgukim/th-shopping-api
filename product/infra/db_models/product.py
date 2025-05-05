a29c1f9c84f71ba6e75a0f5928006ef4bdd9a38b
takgukim <ceman08071039@gmail.com>
2025-05-05 20:00:43 +0900

    제품 테이블에 unsigned 수정 (마이그레이션 수동 적용방법도 있음 - 추후 사용할것)

--- product/infra/db_models/product.py
+++ product/infra/db_models/product.py
@@ -1,4 +1,9 @@
-from sqlalchemy import String, Integer
+#from sqlalchemy import String, INTEGER
+
+# mysql의 unsigned를 써야하기 때문에
+from sqlalchemy import String
+from sqlalchemy.dialects.mysql import INTEGER 
+
 from sqlalchemy.orm import Mapped, mapped_column
 from database import Base
 
@@ -6,7 +11,6 @@ class Product(Base):
 
     __tablename__ = "th_products"
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    id: Mapped[int] = mapped_column(INTEGER(unsigned=True), primary_key=True, autoincrement=True)
     code: Mapped[str] = mapped_column(String(8), nullable=False, index=True) 
     name: Mapped[str] = mapped_column(String(30), nullable=False, index=True)
-
